1438044107b40180cb011604bd5cf536dc29f1bf
aijiang <azt0903@163.com>
2025-04-28 00:05:13 +0800

    feat: 添加设计模式-访问者模式

---
+++ src/com/designpattern/VisitorPattern.java
@@ -0,0 +1,115 @@
+package com.designpattern;
+
+import java.util.Scanner;
+
+/**
+ * 访问者模式
+ *
+ * @author Az
+ * @date 2025/4/27
+ */
+public class VisitorPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        ShapeV[] shapes = new ShapeV[n];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            if ("Circle".equals(input[0])) {
+                int radius = Integer.parseInt(input[1]);
+                shapes[i] = new CircleV(radius);
+            } else if ("Rectangle".equals(input[0])) {
+                int width = Integer.parseInt(input[1]);
+                int length = Integer.parseInt(input[2]);
+                shapes[i] = new RectangleV(width, length);
+            } else {
+                System.out.println("Invalid input");
+                return;
+            }
+        }
+        Drawing drawing = new Drawing(shapes);
+        Visitor areaCalculator = new AreaCalculator();
+        drawing.accept(areaCalculator);
+    }
+}
+
+interface ShapeV {
+    void accept(Visitor visitor);
+}
+
+class CircleV implements ShapeV {
+    private final int radius;
+
+    public CircleV(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class RectangleV implements ShapeV {
+    private final int width;
+    private final int length;
+
+    public RectangleV(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+interface Visitor {
+    void visit(CircleV circle);
+
+    void visit(RectangleV rectagle);
+}
+
+class AreaCalculator implements Visitor {
+
+    @Override
+    public void visit(CircleV circle) {
+        double area = 3.14 * circle.getRadius() * circle.getRadius();
+        System.out.println(area);
+    }
+
+    @Override
+    public void visit(RectangleV rectagle) {
+        double area = rectagle.getLength() * rectagle.getWidth();
+        System.out.println(area);
+    }
+}
+
+class Drawing {
+    private final ShapeV[] shapes;
+
+    public Drawing(ShapeV[] shapes) {
+        this.shapes = shapes;
+    }
+
+    public void accept(Visitor visitor) {
+        for (ShapeV shape : shapes) {
+            shape.accept(visitor);
+        }
+    }
+}
